ea4d692fe88dcdfd1fa5eb8ed0fc599d13780137
Anoob Bava <anoob.bava@gmail.com>
2019-04-15 00:01:58 +0530

    added functionalities for the count

--- src/components/ButtonCount.vue
+++ src/components/ButtonCount.vue
@@ -1,13 +1,22 @@
 <template>
-  <h3>welcome to button count application</h3>
+  <div>
+    <h3>welcome to button count application</h3>
+    <p>Button count is: {{count}}</p>
+    <button @click="changeCount">click</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+data () {
+  return {
+    count: 0
+  }
+},
+methods: {
+  changeCount () {
+    this.count += 1
+  }
+  }
 }
 </script>
-
-<style>
-
-</style>
